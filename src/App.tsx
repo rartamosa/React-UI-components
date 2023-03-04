92141df6739ac4cd93aabd2d29b52d3991ae9c53
Marta Rosa <marta.r.93@gmail.com>
2023-03-04 15:56:34 +0100

    Ready for code review

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from "react";
-// @ts-ignore
-
-import { faSun } from "@fortawesome/free-solid-svg-icons";
 
 // TAGS INPUT
 import TagInput from "./components/TagsInput/TagsInput";
@@ -61,33 +58,10 @@ const App = () => {
         onTagRemove={(tagToRemove) =>
           setTags(tags.filter((tag) => tag !== tagToRemove))
         }
-        // containerBorderColor="blue"
-        // containerBorderWidth="3px"
-        // componentSize="large"
-        // tagsSuggestionsBorderColor="green"
-        // tagsSuggestionsBorderWidth="2px"
-        // backgroundColor="yellow"
-        // placeholder="Hello"
-        // tagsFontColor="purple"
-        // errorBorderColor="red"
-        // isDisabled={true}
-        // tagsSuggestionsContainerPosition="top"
       />
 
       {/* SELECT */}
-      <Select
-        selectOptions={selectDummyData}
-        shouldCloseOnClear={false}
-        // placeholder="Hi there!"
-        // containerBorderColor="green"
-        // containerBorderWidth="3px"
-        // componentSize="large"
-        // backgroundColor="pink"
-        // isDisabled={true}
-        // errorBorderColor="red"
-        // iconColor="blue"
-        // customIcon="sun"
-      />
+      <Select selectOptions={selectDummyData} shouldCloseOnClear={false} />
 
       {/* DIALOG */}
       <Button
@@ -105,15 +79,8 @@ const App = () => {
         actionButtonText="Delete"
         cancelButtonText="Cancel"
         buttonProps={{
-          // border: "4px solid blue",
           fontFamily: "Red Hat Display",
-          // backgroundColor: "pink",
-          // padding: "15px",
         }}
-        // actionButtonColor="red"
-        // cancelButtonColor="blue"
-        // overlayColor="pink"
-        // dialogPosition="right-top"
         blockScroll={true}
         closeOnEsc={true}
         closeOnOverlayClick={true}
@@ -125,52 +92,18 @@ const App = () => {
         onClick={() =>
           onToastAdd({
             toastHeader: "Example toast header",
-            // typeOfToast: "warning",
-            // toastDescription: "Example toast description",
-            // size: "large",
-            // boxShadow: true,
-            // toastIcon: "sun",
-            // iconColor: "red",
-            // toastBacgroundColor: "pink",
-            // toastFontColor: "black",
           })
         }
       >
         Generate toast
       </Button>
-      <Toast
-        toastList={toastList}
-        // animationType="slide"
-        onToastRemove={onToastRemove}
-        // toastsPosition="top-left"
-      />
+      <Toast toastList={toastList} onToastRemove={onToastRemove} />
 
       {/* ACCORDION */}
-      <Accordion
-        accordionBody={accordionDummyData}
-        // iconColor="red"
-        // customIconClosed="sun"
-        // show="many"
-        // accordionBodyHeight="500px"
-        // scroll={false}
-        // containerBackgroundColor="red"
-        // accordionBackgroundColor="pink"
-        // defaultIndex={0}
-        // defaultIndexes={[0, 1]}
-      />
+      <Accordion accordionBody={accordionDummyData} />
 
       {/* TABLE */}
-      <Table
-        columnNames={dummyColumnNames}
-        tableRows={dummyTableRows}
-        // headerBackGroundColor="pink"
-        // denseRows={true}
-        // iconColor="blue"
-        // borderColor="red"
-        // borderWidth="10px"
-        // hoverColor="pink"
-        // allowSorting={false}
-      />
+      <Table columnNames={dummyColumnNames} tableRows={dummyTableRows} />
     </>
   );
 };
